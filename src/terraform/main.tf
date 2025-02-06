01a694df154ae1d6f9947f39f6bb87cffd5b1fa3
Anderson Souza <andersonssilveira96@gmail.com>
2025-02-06 18:29:08 -0300

    Updating TF Files

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -48,16 +48,3 @@ resource "aws_cognito_user_pool_client" "fiapx-usersclient" {
   ]
 }
 
-module "lambda" {
-  source = "./modules/lambda"
-  arn    = data.aws_iam_role.name.arn
-}
-
-module "apiGateway" {
-  source               = "./modules/apiGateway"
-  arn_lambda_auth      = module.lambda.lambda_arn_auth
-  arn_lambda_signup    = module.lambda.lambda_arn_signup
-  environment          = var.environment
-  nome_lambda_auth     = module.lambda.nome_lambda_auth
-  nome_lambda_signup   = module.lambda.nome_lambda_signup
-}
